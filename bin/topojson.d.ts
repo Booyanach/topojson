97741f48c035411136eef1eb34b458cdb44b9256
Gonçalo Vieira <goncalo.vieira@scivisum.co.uk>
2016-05-24 17:21:46 +0100

    added the full topojson interface to the definitions file

--- bin/topojson.d.ts
+++ bin/topojson.d.ts
@@ -1,11 +1,18 @@
 declare module Topojson {
     export interface Base {
-        feature(json: any, collection: any): {features: any};
+        version: string;
+        feature(json: any, collection: any): {features: Array<any>};
+        mesh(topology: any, o: any, filter: Function): {type: any, coordinates: Array<any>};
+        meshArcs(topology: any, o: any, filter: Function): {type: any, coordinates: Array<any>};
+        object(topology: any, o: any): {type: any, coordinates: Array<any>};
+        merge(topology: any): {type: any, coordinates: Array<any>};
+        mergeArcs(topology: any, objects: Array<any>): {type: string, arcs: Array<any>};
+        neighbors(objects: Array<any>): Array<any>;
+        presimplify(topology: any, triangleArea: any): Array<any>;
     }
 }
 
-declare var topojson: Topojson.Base;
-
 declare module "topojson" {
+    var topojson: Topojson.Base;
     export = topojson;
 }
